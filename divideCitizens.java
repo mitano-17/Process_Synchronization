8b22ed9673298e8df06266eeb48179866a1c385e
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-04-02 21:26:32 +0800

    uploaded divideCitizens function

---
+++ divideCitizens.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/* NOTE: Team must compose of at most 2 super citizens and up to 4 members only */
+
+public class divideCitizens {
+    public static void divideCitizens(ArrayList<Integer> regCitQueue, ArrayList<Integer> superCitQueue){
+
+    }
+}
