223567b5dea758ed2b09508997c501146f4433cd
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-04-02 23:31:29 +0800

    Update createGroup.java

--- createGroup.java
+++ createGroup.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 /* NOTE: Team must compose of at most 2 super citizens and up to 4 members only */
 
 public class createGroup {
-    public static void createGroup(int regCitizens, int superCitizens){
+     static void createGroup(){
         /* Create regular citizens and super citizens list */
         /* These lists are for containing the IDs of each citizens */
         /* To be used for outputting phrases */
@@ -11,10 +11,10 @@ public class createGroup {
         ArrayList<Integer> superCitQueue = new ArrayList();
 
         /* Populate lists with integers */
-        for(int i = 0; i < regCitizens; i++){
+        for(int i = 0; i < sharedVar.regCitizens; i++){
             regCitQueue.add(i);
         }
-        for(int i = 0; i < superCitizens; i++){
+        for(int i = 0; i < sharedVar.superCitizens; i++){
             superCitQueue.add(i);
         }
 
