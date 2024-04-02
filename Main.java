8eb43bcf199842ef16c0094b5f1402982f856eaf
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-04-02 20:48:53 +0800

    added array lists

--- Main.java
+++ Main.java
@@ -5,7 +5,25 @@ public class Main {
 
     /* NOTE: Team must compose of at most 2 super citizens and up to 4 members only */
     public static void createGroup(int regCitizens, int superCitizens){
-        System.out.println(regCitizens + " " + superCitizens);
+        //System.out.println(regCitizens + " " + superCitizens);
+
+        /* Create regular citizens and super citizens list */
+        /* These lists are for containing the IDs of each citizens */
+        /* To be used for outputting phrases */
+        ArrayList<Integer> regCitQueue = new ArrayList();
+        ArrayList<Integer> superCitQueue = new ArrayList();
+
+        /* Populate lists with integers */
+        for(int i = 0; i < regCitizens; i++){
+            regCitQueue.add(i);
+        }
+        for(int i = 0; i < superCitizens; i++){
+            superCitQueue.add(i);
+        }
+
+        System.out.print(regCitQueue);
+        System.out.println();
+        System.out.print(superCitQueue);
     }
 
     public static void main(String[] args)
