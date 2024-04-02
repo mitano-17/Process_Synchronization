06e7c165932e56739d7fb63eaa210f24a8a69645
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-04-02 21:41:12 +0800

    added throw exception

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         String name,line;
         String[] split;
