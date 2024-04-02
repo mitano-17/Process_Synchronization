1173183256051135d0de82d454ec6de8380caf8c
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-04-02 20:53:19 +0800

    removed createGroup function

--- Main.java
+++ Main.java
@@ -3,29 +3,6 @@ import java.io.*;
 
 public class Main {
 
-    /* NOTE: Team must compose of at most 2 super citizens and up to 4 members only */
-    public static void createGroup(int regCitizens, int superCitizens){
-        //System.out.println(regCitizens + " " + superCitizens);
-
-        /* Create regular citizens and super citizens list */
-        /* These lists are for containing the IDs of each citizens */
-        /* To be used for outputting phrases */
-        ArrayList<Integer> regCitQueue = new ArrayList();
-        ArrayList<Integer> superCitQueue = new ArrayList();
-
-        /* Populate lists with integers */
-        for(int i = 0; i < regCitizens; i++){
-            regCitQueue.add(i);
-        }
-        for(int i = 0; i < superCitizens; i++){
-            superCitQueue.add(i);
-        }
-
-        System.out.print(regCitQueue);
-        System.out.println();
-        System.out.print(superCitQueue);
-    }
-
     public static void main(String[] args)
     {
         String name,line;
@@ -45,7 +22,7 @@ public class Main {
             superCitizens = sc.nextInt();
             sc.close();
 
-            createGroup(regCitizens, superCitizens);
+            createGroup.createGroup(regCitizens, superCitizens);
         }
         catch(Exception e) {
             System.out.println("Invalid data type");
